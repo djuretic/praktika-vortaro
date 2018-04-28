cbaba8df26554bb4756624fc60afe37d4a6edab7
Dušan Juretić <dusan.juretic@gmail.com>
2018-04-27 22:57:43 -0300

    Bump version

--- app/src/main/java/com/esperantajvortaroj/app/DatabaseHelper.kt
+++ app/src/main/java/com/esperantajvortaroj/app/DatabaseHelper.kt
@@ -8,7 +8,7 @@ import java.util.ArrayList
 class DatabaseHelper : SQLiteAssetHelper {
     companion object {
         val DB_NAME = "vortaro.db"
-        val DB_VERSION = 4
+        val DB_VERSION = 5
     }
 
     constructor(context: Context) : super(context, DB_NAME, null, DB_VERSION) {
