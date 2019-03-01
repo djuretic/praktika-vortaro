3d3cf725ecb98b8f2cad5fdb23b72076d11644c5
Dušan Juretić <dusan.juretic@gmail.com>
2019-02-28 22:12:32 -0300

    Add new activity to detect if the database was loaded

---
+++ app/src/main/kotlin/com/esperantajvortaroj/app/LoadingActivity.kt
@@ -0,0 +1,27 @@
+package com.esperantajvortaroj.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.esperantajvortaroj.app.db.DatabaseHelper
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        val context = this
+        doAsync {
+            DatabaseHelper.getLanguagesHash(context)
+            uiThread {
+                val intent = Intent(context, SearchActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
+                context.startActivity(intent)
+                context.finish()
+            }
+
+        }
+    }
+}
